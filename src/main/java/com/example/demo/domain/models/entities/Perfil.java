153710d917db402038bbc8ebb55ca56bbfcca599
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-03-14 16:16:11 -0300

    docker compose db, app properties

--- src/main/java/com/example/demo/domain/models/entities/Perfil.java
+++ src/main/java/com/example/demo/domain/models/entities/Perfil.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 
+@Entity
+@Table
 @Data
 public class Perfil {
 
