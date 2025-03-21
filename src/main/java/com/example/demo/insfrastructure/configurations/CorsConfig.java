75786184f1d38ed284b397110786b488df3581fd
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-03-21 13:42:01 -0300

    cors config

---
+++ src/main/java/com/example/demo/insfrastructure/configurations/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.example.demo.insfrastructure.configurations;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+
+		registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+				.allowedMethods("POST", "PUT", "DELETE", "GET").allowedHeaders("*");
+	}
+}
